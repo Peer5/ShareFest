f9849dc21acf7bdaccc5a30dbe8fbddf94d47d3f
pyg <pete.y.gao@gmail.com>
2012-11-13 23:42:48 -0800

    Generates a 'shortened' url link from UTC timestamp (by default, a different seed can be used).

--- rooms.js
+++ rooms.js
@@ -1,8 +1,49 @@
 // an in memory placeholder for the rooms
 var rooms = {};
+var DICTIONARY = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split('');
 
 function getRoom(id) {
     if (id in rooms)
+}
+
+function generateID(seed) {
+    /* Given a seed, this function will return a 'shortened' URL */
+    if(typeof(seed)==='undefined') seed = new Date().getTime();
+    return encode(seed);
+}
+
+function getSeedFromID(id) {
+    /* Given a 'shortened' URL, it will return the seed */
+    if(typeof(seed)==='undefined') return -1;
+    return decode(id);
+}
+
+function encode(i) {
+    /* Generates a 'shortened' URL */
+    if (i == 0) return DICTIONARY[0];
+
+    var result = '';
+    var base = DICTIONARY.length;
+
+    while (i > 0) {
+        result += DICTIONARY[i % base];
+        i = i / base;
+    }
+
+    result = result.reverse();
+    return result;
+}
+
+function decode(i) {
+    /* Returns the original input from a 'shortened' URL */
+    var i = 0;
+    var base = DICTIONARY.length;
+    
+    input.split("").forEach(function(c){
+    i = i * base + DICTIONARY.indexOf(c);
+    });
+    
+    return i;
 }
 
 exports = rooms;
