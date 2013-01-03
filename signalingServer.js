1faf01369be47b36829f1025309682cd8c918114
Hadar Weiss <whadar@gmail.com>
2013-01-03 16:11:57 +0200

    socket.io organizing

--- signalingServer.js
+++ signalingServer.js
@@ -5,51 +5,54 @@
  * Time: 22:10
  * To change this template use File | Settings | File Templates.
  */
-var io = require('socket.io').listen(8080);
+var socketio = require('socket.io');
+var io;
 //var rooms = require('rooms.js');
 
-io.sockets.on('connection', function (socket) {
-    socket.emit('connection', {});
+exports.start = function (server) {
+    var io = socketio.listen(server);
+    io.sockets.on('connection', function (socket) { /*of('room').*/
+        socket.emit('connection', {});
 
-    socket.on('join',function(msg){
-        users[users.length] = socket.id;
+        socket.on('join', function (msg) {
+            users[users.length] = socket.id;
+        });
+
+        socket.on('offer', function (msg) {
+            socket.broadcast.emit('offer', message);
+
+//            for (var i = 0; i < users.length; ++i) {
+//                if (users[i] != socket.id) {        //publishing the offer to all other users
+//                    this.sendOffer(socket.id, msg);
+//                }
+//            }
+        });
+
+        socket.on('answer', function (msg) {    //msg = {socketid:...,data:...}
+            this.answer(msg.socketid, msg);
+        });
+
+        socket.on('disconnect', function (msg) {
+
+
+        });
     });
-
-    socket.on('offer', function (msg) {
-        for(var i=0;i<users.length;++i){
-            if(users[i]!=socket.id){        //publishing the offer to all other users
-                this.sendOffer(socket.id,msg);
-            }
-        }
-    });
-
-    socket.on('answer', function (msg) {    //msg = {socketid:...,data:...}
-        this.answer(msg.socketid,msg);
-    });
-
-    socket.on('disconnect', function (msg) {
-    });
-
-    socket.on('sdp',function(msg){
-
-    });
-});
-
-this.send = function (socketId, message) {
+}
+exports.send = function (socketId, message) {
     var s = io.sockets.sockets[socketId];
     if (s) {
         s.emit('send', message);
     }
 };
 
-this.sendOffer = function (socketId, message) {
+exports.sendOffer = function (socketId, message) {
     var s = io.sockets.sockets[socketId];
     if (s) {
         s.emit('offer', message);
     }
 };
 
-this.answer = function (socketId, message) {
+exports.answer = function (socketId, message) {
     var s = io.sockets.sockets[socketId];
     if (s) {
         s.emit('answer', message);
