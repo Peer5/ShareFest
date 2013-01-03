450caaf229605ee3d466ceef8f839a64c46da8fc
shacharz <shacharz@gmail.com>
2013-01-03 14:52:11 +0200

    chrome example: starting to signal to our signalingServer

---
+++ chromeExample/wsConnection.js
@@ -0,0 +1,29 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Shachar
+ * Date: 03/01/13
+ * Time: 12:41
+ * To change this template use File | Settings | File Templates.
+ */
+(function () {
+    WsConnection = function (wsServerUrl, clientId) {
+        var thi$ = this;
+        this.socket;
+        this.initiateWebSocket(wsServerUrl,clientId)
+    };
+
+    WsConnection.prototype = {
+
+        initiateWebSocket:function (wsServerUrl, clientId) {
+            var thi$ = this;
+            this.socket = io.connect(wsServerUrl);
+            console.log('new websocket');
+            this.socket.on('connect',function(){
+                this.socket.on('disconnect', function () {
+                });
+                this.socket.on('message',function(){
+                });
+            });
+        }
+    };
+})();
