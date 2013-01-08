5a84a4a03d39686567db14a116bc47760dd6a975
shacharz <shacharz@gmail.com>
2013-01-08 18:42:03 +0200

    some logs

--- server.js
+++ server.js
@@ -15,12 +15,14 @@ app.use(express.static(__dirname + '/public'));
 
 app.configure('development', function () {
     app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+    console.log('listening to port 13337')
     server = app.listen(13337);
     signaling.start(server);
     console.log('here I am');
 });
 
 app.configure('production', function () {
+    console.log('listening to port 80')
     server = app.listen(80); //nodejitsu will map this to 80
     signaling.start(server);
 });
