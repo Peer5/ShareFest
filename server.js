8589881fb6dfae249c1f969e835aeb71dd8db463
Hadar Weiss <whadar@gmail.com>
2013-08-13 15:40:08 +0300

    added tls support added faq and press pages

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var signaling = require('./server/lib/signalingServer.js');
 var router = require('./server/lib/router.js')
-var server;
+var https = require('https');
+var fs = require('fs');
 
 var allowCrossDomain = function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -10,20 +11,45 @@ var allowCrossDomain = function (req, res, next) {
     next();
 }
 
+if (process.env.REQUIRE_HTTPS) {
+    app.use(function (req, res, next) {
+        if (!req.secure) {
+            return res.redirect('https://' + req.get('Host') + req.url);
+        }
+        next();
+    });
+}
+
+app.use(express.json());
+app.use(express.compress());
 app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/public'));
 
+var server;
+
 app.configure('development', function () {
     app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
-    console.log('listening to port 13337')
+    console.log('listening to port 13337');
     server = app.listen(13337);
     signaling.start(server);
     console.log('here I am');
 });
 
 app.configure('production', function () {
-    console.log('listening to port 80')
-    server = app.listen(80); //nodejitsu will map this to 80
+    var options = {
+        key:fs.readFileSync('secret/private.key'),
+//        ca:[fs.readFileSync('secret/AddTrustExternalCARoot.crt'),
+//            fs.readFileSync('secret/SSLcomAddTrustSSLCA.crt')],
+        cert:fs.readFileSync('secret/self.crt')
+    };
+    app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+
+    console.log('listening to port 80');
+    server80 = app.listen(80);
+
+    console.log('listening to port 443');
+    server = https.createServer(options, app).listen(443);
+
     signaling.start(server);
 });
 
--- server/lib/router.js
+++ server/lib/router.js
@@ -1,29 +1,34 @@
 var rooms = require('./rooms.js');
 
-exports.configure = function(app, rootdir) {
+exports.configure = function (app, rootdir) {
     app.get('/browser', function (req, res) {
         res.sendfile(rootdir + '/public/browser.html');
     });
 
+    app.get('/faq', function (req, res) {
+        res.sendfile(rootdir + '/public/faq.html');
+    });
+
+
+    app.get('/press', function (req, res) {
+        res.sendfile(rootdir + '/public/press.html');
+    });
+
     app.get('/about', function (req, res) {
-        res.redirect("https://github.com/peer5/sharefest");
+        res.redirect("https://github.com/peer5/sharefest#about");
     });
 
     app.get('/contact', function (req, res) {
-        res.redirect("https://github.com/peer5/sharefest");
+        res.redirect("mailto://sharefest@peer5.com");
     });
 
     app.get('/issues', function (req, res) {
-        res.redirect("https://github.com/peer5/sharefest");
+        res.redirect("https://github.com/peer5/sharefest/issues?page=1&state=open");
     });
 
     app.get('/:id', function (req, res) {
-//        var roomId = req.params.id;
-//        var room = rooms.getRoom(roomId);
-//    displayRoom(room);
-
         //todo: bind the room info to the page and output
         res.sendfile(rootdir + '/public/index.html');
-        //res.send(room);
     });
-};
+}
+;
