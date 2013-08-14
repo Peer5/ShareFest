bfec6a715e2c89c3cf8cfbf96b025c24973fd35b
Hadar Weiss <whadar@gmail.com>
2013-08-14 13:25:12 +0300

    require https in production disable dynamic port and server for ws

--- sharefest/run-prod.sh
+++ sharefest/run-prod.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export NODE_ENV=production
-export WS_SERVER="`curl http://169.254.169.254/latest/meta-data/public-ipv4`"
-export WS_PORT=443
-
+#export WS_SERVER="`curl http://169.254.169.254/latest/meta-data/public-ipv4`"
+#export WS_PORT=443
+export REQUIRE_HTTPS=1
 NOW=$(date +"%F")
 forever stop server.js
 forever start -e err.log -o out.log -l $NOW.log --append server.js
